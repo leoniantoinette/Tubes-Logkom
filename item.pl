31c6c935f24372529ad2dd9a88ff13a4605839ad
VionieNovencia <16520188@std.stei.itb.ac.id>
2021-11-21 00:59:59 +0700

    item 1.0

---
+++ item.pl
@@ -0,0 +1,22 @@
+ternak('Chicken').
+ternak('Cow').
+ternak('Pig').
+
+hasil_ternak('Egg').
+hasil_ternak('Milk').
+hasil_ternak('Bacon').
+
+seed('Carrot seed').
+seed('Corn seed').
+seed('Tomato seed').
+
+hasil_tani('Carrot').
+hasil_tani('Corn').
+hasil_tani('Tomato').
+
+ikan('Tuna').
+ikan('Salmon').
+ikan('Trout').
+
+equipment('shovel').
+equipment('fishing rod').
