81d88ef621240965dc410b19a1f563a54e7bd13d
chryes220 <christinehutabarat12@gmail.com>
2021-11-25 18:50:06 +0700

    fix farm.pl

--- farm.pl
+++ farm.pl
@@ -1,14 +1,19 @@
+/* TO DO :
+    tambahin exp
+    panggil update inventory aja
+    setelah harvest update quest
+    setelah setiap activity tambahin move */
+
 :- include('item.pl').
 :- include('map.pl').
-:- include ('inventory.pl').
+:- include('inventory.pl').
 
-:- dynamic(crop_stat/4) % lokasi dan umur crop
+:- dynamic(crop_stat/4). % lokasi dan umur crop
 % crop_stat(plant, absis, ordinat, age)
 /* List of plants :
     carrot_plant
     corn_plant
     tomato_plant */
-
 
 % metode untuk melakukan dig/penggalian
 dig :-
@@ -27,6 +32,21 @@ dig :-
     assertz(pos_digged(X,Y)),
     write('You digged the tile.')).
 
+plantCarrot :-
+    inventory('carrot seed', A),
+    ((A > 0 -> (update_inventory('carrot seed', -1), write('You planted a carrot seed.\n')));
+    (write('You don\'t have any carrot seed! Buy it first from the market!'))).
+
+plantCorn :-
+    inventory('corn seed', A),
+    ((A > 0 -> update_inventory('corn seed', -1), write('You planted a corn seed.\n'));
+    (write('You don\'t have any corn seed! Buy it first from the market!'))).
+
+plantTomato :-
+    inventory('tomato seed', A),
+    ((A > 0 -> update_inventory('tomato seed', -1), write('You planted a tomato seed.\n'));
+    (write('You don\'t have any tomato seed! Buy it first from the market!'))).
+
 plant :- 
     in_game(false),
     !,
@@ -37,31 +57,31 @@ plant :-
     posisi(X,Y),
     \+pos_digged(X,Y), !, write('The tile is not digged yet.').
 plant :-
-    write('You have : \n')
-    forall((seed(X), inventory(X,Y), Y > 0), format('- ~w ~w ~n', [Y, X])),
+    write('You have : \n'),
+    forall((seed(X), inventory(X,Y)), format('- ~w ~w ~n', [Y, X])),
     read(In),
-    In = 'carrot', 
-    inventory('carrot seed', W), W > 0, 
-    write('You planted a carrot seed.\n')
-    retract(inventory('carrot seed', W)),
-    assertz(inventory('carrot seed', W-1)),
-    assertz(crop_stat(carrot_plant, X, Y, 0)).
-plant :-
-    In = 'corn',
-    inventory('corn seed', U), U > 0, 
-    write('You planted a corn seed.\n')
-    retract(inventory('corn seed', U)),
-    assertz(inventory('corn seed', U-1)),
-    assertz(crop_stat(corn_plant, X, Y, 0)).
-plant :-
-    In = 'tomato',
-    inventory('tomato seed', V), V > 0, 
-    write('You planted a tomato seed.\n')
-    retract(inventory('tomato seed', V)),
-    assertz(inventory('tomato seed', V-1)),
-    assertz(crop_stat(tomato_plant, X, Y, 0)).
-plant :-
-    write('There is no such seed in your inventory.\n').
+    ((In == 'carrot' -> plantCarrot);
+    (In == 'corn' -> plantCorn);
+    (In == 'tomato' -> plantTomato);
+    (write('There is no such seed in your inventory.\n'))).
+
+harvestCarrot :-
+    posisi(X,Y),
+    crop_stat(carrot_plant,X,Y,A),
+    ((A >= 14 -> write('You harvested carrot.\n'), update_inventory('carrot', 1));
+    (write('Your carrot is not ready to be harvested yet!\n'))).
+
+harvestCorn :-
+    posisi(X,Y),
+    crop_stat(corn_plant,X,Y,A),
+    ((A >= 12 -> write('You harvested corn.\n'), update_inventory('corn', 1));
+    (write('Your corn is not ready to be harvested yet!\n'))).
+
+harvestTomato :-
+    posisi(X,Y),
+    crop_stat(tomato_plant,X,Y,A),
+    ((A >= 20 -> write('You harvested tomato.\n'), update_inventory('tomato', 1));
+    (write('Your tomato is not ready to be harvested yet!\n'))).
 
 harvest :- 
     in_game(false),
@@ -71,22 +91,7 @@ harvest :-
 harvest :-
     in_game(true),
     posisi(X,Y),
-    crop_stat(carrot_plant,X,Y,W), W >= 14,
-    write('You harvested carrot.\n'),
-    retract(crop_stat(carrot_plant,X,Y,W)),
-    retract(inventory('carrot',U)), 
-    assertz(inventory('carrot',U+1)).
-harvest :-
-    crop_stat(corn_plant,X,Y,V), V >= 12,
-    write('You harvested corn.\n'),
-    retract(crop_stat(corn_plant,X,Y,V)),
-    retract(inventory('corn',Z)), 
-    assertz(inventory('corn',Z+1)).
-harvest :-
-    crop_stat(tomato_plant,X,Y,A), A >= 20,
-    write('You harvested tomato.\n'),
-    retract(crop_stat(tomato_plant,X,Y,A)),
-    retract(inventory('tomato',B)), 
-    assertz(inventory('tomato',B+1)).
-harvest :-
-    write('Nothing can be harvested yet.\n').
+    ((crop_stat(carrot_plant,X,Y,_) -> harvestCarrot);
+    (crop_stat(corn_plant,X,Y,_) -> harvestCorn);
+    (crop_stat(tomato_plant,X,Y,_) -> harvestTomato);
+    (write('There is no plant here.'))).
